8410c578fe8ee8809b05a3910f0a890e7147ea93
Sudipta Saha Niloy <56694094+SudiptaSahaNiloy@users.noreply.github.com>
2022-04-23 22:27:07 +0600

    search, filter and pagination added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,9 +1,10 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apiFeatures");
 
 // Create a book
-exports.createBook = catchAsyncErrors(async (req, res, next) => { 
+exports.createBook = catchAsyncErrors(async (req, res, next) => {
     //catchAsyncError will the the whole function as an input and execute try and catch functionality
     const book = await Product.create(req.body);
 
@@ -15,11 +16,19 @@ exports.createBook = catchAsyncErrors(async (req, res, next) => {
 
 // Get all books
 exports.getAllBooks = catchAsyncErrors(async (req, res, next) => {
-    const books = await Product.find();
+    const resultPerPage = 5;
+    const bookCount = await Product.countDocuments();
+    const apiFeature = new ApiFeatures(Product.find(), req.query)
+        .search()
+        .filter()
+        .pagination(resultPerPage)
+
+    const books = await apiFeature.query;
 
     res.status(200).json({
         success: true,
         books,
+        bookCount,
     })
 })
 
@@ -34,6 +43,20 @@ exports.getSingleBook = catchAsyncErrors(async (req, res, next) => {
     res.status(200).json({
         success: true,
         book
+    })
+})
+
+exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
+    const book = await Product.findById(req.params.id);
+
+    if (!book) {
+        return next(new ErrorHandler("Book not found", 500))
+    }
+
+    res.status(200).json({
+        success: true,
+        book,
+        bookCount,
     })
 })
 
